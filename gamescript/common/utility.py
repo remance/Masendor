d382b55cdc4ee6699abccdc13e69e73658a296e7
Remance <be-gone@hotmail.com>
2022-08-09 18:51:18 +0700

    Animation and sprite update
    - Change weapon sprite and animation to have separate base image for either main or sub weapon. This indicates the direction that player see the weapon (outward or inward).

--- gamescript/common/utility.py
+++ gamescript/common/utility.py
@@ -290,12 +290,12 @@ def popout_lorebook(self, section, game_id):
     used for when user right click at icon that has encyclopedia section"""
     self.game_state = "menu"
     self.battle_menu.mode = "encyclopedia"
-    self.battle_ui_updater.add(self.encyclopedia, self.lore_name_list, self.lore_scroll, *self.lore_button_ui)
+    self.battle_ui_updater.add(self.encyclopedia, self.lore_name_list, self.lore_name_list.scroll, *self.lore_button_ui)
 
-    self.encyclopedia.change_section(section, self.lore_name_list, self.subsection_name, self.lore_scroll, self.page_button,
+    self.encyclopedia.change_section(section, self.lore_name_list, self.subsection_name, self.lore_name_list.scroll, self.page_button,
                                      self.battle_ui_updater)
     self.encyclopedia.change_subsection(game_id, self.page_button, self.battle_ui_updater)
-    self.lore_scroll.change_image(new_row=self.encyclopedia.current_subsection_row)
+    self.lore_name_list.scroll.change_image(new_row=self.encyclopedia.current_subsection_row)
 
 
 def popup_list_open(self, new_rect, new_list, ui_type):
